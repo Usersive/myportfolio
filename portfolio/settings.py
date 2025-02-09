c09b80a1d3ba9faf778c972180d6c09a8da45a69
Usersive <lawalusersive@gmail.com>
2025-02-09 17:01:14 +0100

    first

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -39,7 +39,7 @@ DEBUG = os.environ.get('DEBUG', 'True')=="True"
 # DEBUG = True
 
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://lawalportfolio.onrender.com',]
+ALLOWED_HOSTS = ['127.0.0.1', 'lawalportfolio.onrender.com',]
 
 # Application definition
 

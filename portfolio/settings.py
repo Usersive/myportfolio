5c449ccfe4a09a176a64e10429cece810e364337
Usersive <lawalusersive@gmail.com>
2025-02-09 21:25:09 +0100

    collect

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -163,8 +163,8 @@ STATIC_ROOT=BASE_DIR /'static'
 # STATICFILES_DIRS=[
 #     'portfolio/static',
 # ]
-STATICFILES_DIR     = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATICFILES_DIR     = [os.path.join(BASE_DIR, 'static')]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 MEDIA_URL = '/media/'
